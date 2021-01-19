cd3d25a1351dfcb5539d0f28956a79545e4a1349
João Marcos <jmarcossferreira@hotmail.com>
2021-01-18 21:10:11 -0300

    Commit inicial

---
+++ components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand>PManager</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+
+          <b-nav-item >
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+            </svg>
+            Adicionar sprint
+          </b-nav-item>
+
+          <b-nav-item-dropdown text="João Marcos" right>
+            <b-dropdown-item>
+              Sair
+            </b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <Dashboard />
+    <tabela-sprint/>
+  </div>
+</template>
+
+<script>
+import Dashboard from '~/components/Dashboard.vue'
+import TabelaSprint from '~/components/TabelaSprint.vue'
+export default {
+  components: { Dashboard, TabelaSprint },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+* {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+}
+
+</style>
