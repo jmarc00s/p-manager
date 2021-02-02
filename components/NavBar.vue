5f8df7a5502cde25f50ea03b2c7966652982845a
João Marcos <jmarcossferreira@hotmail.com>
2021-02-02 13:57:56 -0300

    Início da implementação de adição de sprints

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,19 +1,39 @@
 <template>
   <div>
+
+    <!-- MODAL Adicionar sprint -->
+
+    <b-modal id="adicionar-sprint" title="Adicionar sprint">
+      <b-form @submit="submeterDadosSprint">
+        <b-form-group>
+          <b-form-input
+            placeholder="Número da sprint"
+            v-model.number="adicinarSprintForm.numeroSprint"
+            type="text"
+            required>
+
+          </b-form-input>
+        </b-form-group>
+      </b-form>
+
+      {{ adicinarSprintForm }}
+    </b-modal>
+
+
+    <!-- MODAL Adicionar sprint -->
+
+
     <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-brand>PManager</b-navbar-brand>
-
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-
-          <b-nav-item >
+          <b-nav-item @click="adicionarSprint">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
               <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
             </svg>
             Adicionar sprint
           </b-nav-item>
-
           <b-nav-item-dropdown text="João Marcos" right>
             <b-dropdown-item>
               Sair
@@ -27,9 +47,23 @@
 
 <script>
 export default {
+  methods: {
+    adicionarSprint(event) {
+      event.preventDefault();
+      this.$bvModal.show('adicionar-sprint');
+    },
+    submeterDadosSprint() {
+      console.log(this.adicinarSprintForm);
+    }
+  },
   data() {
     return {
-
+      adicinarSprintForm: {
+        numeroSprint: null,
+        pontosEntregues: null,
+        historiasHomologacas: null,
+        erros: null
+      }
     }
   }
 }
--- store/index.js
+++ store/index.js
@@ -1,12 +1,4 @@
 
-const data = [
-  {sprint: 42, pontos: 16, homologadas: 3, erros: 0},
-  {sprint: 43, pontos: 14, homologadas: 3, erros: 1},
-  {sprint: 44, pontos: 14, homologadas: 3, erros: 0},
-  {sprint: 45, pontos: 14, homologadas: 3, erros: 0},
-  {sprint: 46, pontos: 14, homologadas: 3, erros: 0}
-]
-
 export const state = () => ({
   sprints: []
 })
@@ -18,13 +10,18 @@ export const mutations = {
 }
 
 export const actions = {
+
+  adicionarSprint: ({commit}, sprint) => {
+    if(!sprint) return;
+    commit('adicionar', sprint)
+  },
   adicionarVarios: ({commit}, sprints) =>  {
     if(!sprint.length) return;
 
     sprints.forEach(sprint => commit('adicionar', sprint));
   },
   carregarDados: ({commit}) => {
-    const sprints = data;
+    const sprints = [];
     sprints.forEach(sprint => commit('adicionar', sprint));
   }
 }
